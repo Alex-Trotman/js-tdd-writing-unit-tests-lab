d0c5321742cff85133fc680768963d00cb1eecf2
Alex Trotman <alextrotman04@gmail.com>
2023-11-16 11:15:02 -0600

    passes

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,11 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe('isPalindrome', () => {
+    it("Returns true if a word that reads the same forwards and backwards and returns false otherwise", () => {
+        const word = "racecar"
+        const reverse = isPalindrome(word);
+
+        expect(reverse).toBe(true);
+    })
+})
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,8 @@
 // Your code here
+export function isPalindrome(word){
+    let reverse = word.split("").reverse().join("");
+
+    if (word === reverse){
+        return true
+    } else return false
+}
